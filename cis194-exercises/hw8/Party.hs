ee0b22bec0658f0d427c392ec109d34e8b210f3f
Ben Thorne <benjamin.thorne@tufts.edu>
2021-02-26 11:17:41 -0500

    adjusted glAppend and nextLevel

--- cis194-exercises/hw8/Party.hs
+++ cis194-exercises/hw8/Party.hs
@@ -7,8 +7,7 @@ glCons :: Employee -> GuestList -> GuestList
 glCons e (GL es f) = GL (e:es) (f + empFun e)
 
 glAppend :: GuestList -> GuestList -> GuestList
-glAppend gl (GL (e:es) f) = glAppend (glCons e gl) (GL es f)
-glAppend gl (GL [] _)     = gl
+glAppend (GL g1s f1) (GL g2s f2) = GL (g1s ++ g2s) (f1 + f2)
 
 instance Semigroup GuestList where
   (<>) = glAppend
@@ -28,10 +27,12 @@ treeFoldl f acc t = f (foldl' (treeFoldl f) acc (subForest t)) t
 treeFoldr :: (Tree a -> b -> b) -> b -> Tree a -> b
 treeFoldr f acc t = f t $ foldr (flip $ treeFoldr f) acc (subForest t)
 
--- I need to re-think this using the actual fun scores of each GuestList
-nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList,GuestList)
-nextLevel bob ((wBoss,noBoss):gls) = foldr combine (glCons bob noBoss,wBoss) gls
-  where combine (wE,nE) (wBob,noBob) = (glAppend nE wBob, glAppend wE noBob)
+pairMap :: (a -> b) -> (a,a) -> (b,b)
+pairMap f (x,y) = (f x, f y)
+
+nextLevel :: Employee -> [(GuestList,GuestList)] -> (GuestList,GuestList)
+nextLevel boss = formGLs . pairMap mconcat . unzip
+  where formGLs (bosses,noboss) = (glCons boss noboss, moreFun bosses noboss)
 
 maxFun :: Tree Employee -> GuestList
-maxFun
+maxFun = undefined
