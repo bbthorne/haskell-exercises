380cba76e41061d59620035222bc5c48dc441c6d
Ben Thorne <benjamin.thorne@tufts.edu>
2021-02-26 16:54:28 -0500

    finished hw8

--- cis194-exercises/hw8/Party.hs
+++ cis194-exercises/hw8/Party.hs
@@ -2,9 +2,10 @@ module Party where
 import Employee
 import Data.Tree
 import Data.List
+import System.Environment
 
 glCons :: Employee -> GuestList -> GuestList
-glCons e (GL es f) = GL (e:es) (f + empFun e)
+glCons g (GL gs f) = GL (g:gs) (f + empFun g)
 
 glAppend :: GuestList -> GuestList -> GuestList
 glAppend (GL g1s f1) (GL g2s f2) = GL (g1s ++ g2s) (f1 + f2)
@@ -17,9 +18,7 @@ instance Monoid GuestList where
   mappend = glAppend
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun gl1 gl2
-  | gl1 > gl2 = gl1
-  | otherwise = gl2
+moreFun = max
 
 treeFoldl :: (b -> Tree a -> b) -> b -> Tree a -> b
 treeFoldl f acc t = f (foldl' (treeFoldl f) acc (subForest t)) t
@@ -27,12 +26,28 @@ treeFoldl f acc t = f (foldl' (treeFoldl f) acc (subForest t)) t
 treeFoldr :: (Tree a -> b -> b) -> b -> Tree a -> b
 treeFoldr f acc t = f t $ foldr (flip $ treeFoldr f) acc (subForest t)
 
-pairMap :: (a -> b) -> (a,a) -> (b,b)
-pairMap f (x,y) = (f x, f y)
+treeFold :: (a -> [b] -> b) -> Tree a -> b
+treeFold f t = f (rootLabel t) $ map (treeFold f) (subForest t)
 
 nextLevel :: Employee -> [(GuestList,GuestList)] -> (GuestList,GuestList)
-nextLevel boss = formGLs . pairMap mconcat . unzip
-  where formGLs (bosses,noboss) = (glCons boss noboss, moreFun bosses noboss)
+nextLevel boss res = (withBoss,woutBoss)
+  where withBoss = glCons boss $ mconcat (map snd res)
+        woutBoss = mconcat $ map (uncurry moreFun) res
 
 maxFun :: Tree Employee -> GuestList
-maxFun = undefined
+maxFun = uncurry moreFun . treeFold nextLevel
+
+parseGL :: GuestList -> String
+parseGL (GL gs f) = "Total fun: " ++ show f ++ "\n" ++
+                    (unlines . sort) (map empName gs)
+
+main :: IO ()
+main = getArgs >>= readFile . head
+               >>= putStrLn . parseGL . maxFun . read
+
+-- kinda sloppy with do notation:
+mainDo :: IO ()
+mainDo = do
+    args <- getArgs
+    file <- readFile . head $ args
+    putStrLn . parseGL . maxFun . read $ file
