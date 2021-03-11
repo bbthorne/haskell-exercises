1dfcb1008bcc22300a6fca737c505c5862006569
Ben Thorne <benjamin.thorne@tufts.edu>
2021-03-11 13:40:07 -0500

    finished hw12

--- cis194-exercises/hw12/Risk.hs
+++ cis194-exercises/hw12/Risk.hs
@@ -3,7 +3,7 @@
 module Risk where
 
 import Control.Monad.Random
-
+import Data.List
 ------------------------------------------------------------
 -- Die values
 
@@ -26,3 +26,32 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+-- battle simulates one battle in Risk where each side uses the max number of
+-- units possible.
+battle :: Battlefield -> Rand StdGen Battlefield
+battle armies = (rolls attackForce) >>= \a ->
+                (rolls defendForce) >>= \d ->
+                return $ foldr damage armies (zip a d)
+  where attackForce   = min 3 $ attackers armies - 1
+        defendForce   = min 2 $ defenders armies
+        rolls n       = sortBy (flip compare) <$> replicateM n die
+        damage (a,d) b
+          | a > d     = Battlefield (attackers b) (defenders b - 1)
+          | otherwise = Battlefield (attackers b - 1) (defenders b)
+
+-- invade simulates an invasion attempt and runs battle until there are no
+-- defenders or fewer than 2 attackers remaining.
+invade :: Battlefield -> Rand StdGen Battlefield
+invade armies
+  | defenders armies == 0 || attackers armies < 2 = return armies
+  | otherwise = battle armies >>= invade
+
+-- successProb calculates the probability that the attacker will completely
+-- destroy the defender based on 1000 invasion simulations
+successProb :: Battlefield -> Rand StdGen Double
+successProb armies = invasions >>= succProb
+  where numBattles = 1000
+        invasions  = replicateM numBattles $ invade armies
+        succProb   = calcProb . length . filter ((==) 0 . defenders)
+        calcProb n = return $ fromIntegral n / fromIntegral numBattles
